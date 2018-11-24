c61a0fd315fb9dd0984c047b3411d172daf1748d
mattleung10 <44624478+mattleung10@users.noreply.github.com>
2018-11-24 00:09:12 -0500

    Added sys.exit, put try-except when copying images

--- src/task1/gui_3_most_damaged/damaged_panels_imagefiltering.py
+++ src/task1/gui_3_most_damaged/damaged_panels_imagefiltering.py
@@ -23,7 +23,7 @@ def main():
     filename = command_line_args[1]
     #path of the new folder that stores the damaged images
     new_dir = command_line_args[2]
-    #list to store filenames of damaged images
+    #list to store paths of damaged images
     damaged_filenames = []
     #whether or not the directory should be removed (if it already exists)
     remove = False
@@ -32,7 +32,8 @@ def main():
         with open(filename) as json_file: #open file
             data = json.load(json_file) #extracting data in json format
             for p in data["damaged"]:
-                damaged_filenames += [p["filename"]]    
+                #put the paths of the damaged images in the list
+                damaged_filenames += [p["filename"]]
     except:
         print("ERROR: Could not open JSON file")
         return False #end program
@@ -62,19 +63,23 @@ def main():
         os.mkdir(new_dir)
     except OSError:
         print("ERROR: OS Error")
-        return False
+        return False #end program
     else:  
         print("%s was successfully created" % new_dir)
     
     #copy the image files to the new directory
     for i in range(0,len(damaged_filenames),1):
-        #check if image file path is legitimate
-        if os.path.isfile(damaged_filenames[i]) == True:
+        #check if image could be moved
+        try:
             shutil.copy(damaged_filenames[i],new_dir)
-        else:
+        except:
+            #tell user the first file that could not be moved
             print("ERROR: Could not copy %s\nOperation Aborted" % damaged_filenames[i])
             return False #end program
 
     return True #finish program
     
-main()
+if main() == False:
+    sys.exit(-1) #so the caller of your program knows that your program didnt exit properly
+else:
+    sys.exit(0)
