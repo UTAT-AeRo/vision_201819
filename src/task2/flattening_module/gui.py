cce51928f03c793fd0662b8b1992b4900290cad3
Lev McKinney <levmckinney@gmail.com>
2019-01-20 00:56:43 -0500

    updated protection levels and added propertys to gui.py and image_gui.py

--- src/task2/flattening_module/gui.py
+++ src/task2/flattening_module/gui.py
@@ -1,72 +1,208 @@
 from tkinter import *
-from typing import Tuple
+from typing import Tuple, List, Dict
 import nPTransform
 import numpy as np
 from image_gui import Image_GUI
+import argparse
+import os
+import json
 
-# https://stackoverflow.com/questions/5501192/how-to-display-picture-and-get-mouse-click-coordinate-on-it
 
-# TODO fix pTransform aspect ratio issue
-# TODO Allow multiple images
-# TODO Resize the window automatically
-# TODO Implement Reset
-
-class Image_Falattener(Image_GUI):
+class ImageFlattener(Image_GUI):
     """This class represents an the image processing application
         *** This class assumes the solar panels are rectangles ***
 
-    ==== Atributes ===
-    clicks: the clicks that have been made on the canvas
-    save_to: the folder to which the images will be saved.
+    ==== Properties ===
     dim: This is a tuple containing first the length of the smaller
-        edge of the solar panel (width in mm) and then the larger edge
-        (height in mm).
+        edge of the solar panel (width in cm) and then the larger edge
+        (height in cm).
+    saved: a list of the path to all of the _saved images
     """
+    # === Private Attributes ===
+    __dim: Tuple[int, int]
+    __saved: Dict[str, Tuple[int, int]]
+    __clicks: List[Tuple[int, int]]  # the _clicks
+    # that have been made on the canvas since last reload or flattening
+    __imgs_flattened: bool  # the number of panels flattened
+    __num_saved: int  # the number of panels _saved during while processing one
+    # image
 
-    def __init__(self, master: Tk, paths: str, save_to: str, dim: Tuple[int, int]):
+    def __init__(self, master: Tk, paths: str, save_to: str):
         """
         :param master: The root for this Tk window
         :param paths: The paths to each file
         :param save_to: The path to the folder to save images to
-        :param dim: The is a tuple containing first the length of the smaller
-        edge of the solar panel (width) and then the larger edge (height).
         """
-        self.dim = dim
-        self.clicks = []
+        self._img_flattened = False
+        self.__num_saved = 0
+        self.__dim = None
+        self.__clicks = []
+        self.__saved = dict()
+
+        # Toolbar
+        toolbar = Frame(master)
+        reload_button = Button(toolbar, text="Reload",
+                               command=self.reload)
+        save_button = Button(toolbar, text="Save",
+                             command=self.save)
+        next_button = Button(toolbar, text="Next",
+                             command=self.next_file)
+        reload_button.pack(side=LEFT, padx=2, pady=2)
+        save_button.pack(side=LEFT, padx=2, pady=2)
+        next_button.pack(side=LEFT, padx=2, pady=2)
+        toolbar.pack(side=TOP, fill=X)
 
         Image_GUI.__init__(self, master, paths, save_to)
-        self.canvas.bind('<Button-1>', self.left_mouse_down)
+
+        self._canvas.bind('<Button-1>', self.left_mouse_down)
+
+    @property
+    def saved(self):
+        return self.__saved
+
+    @property
+    def dim(self):
+        return self.__dim
 
     def left_mouse_down(self, event):
         """Called whenever the left mouse is clicked on the image canvas"""
+
+        if self._img_flattened:
+            return
+
         x, y = self.to_canvas((event.x, event.y))
         # https://stackoverflow.com/questions/28615900/how-do-i-add-a-mouse-click-position-to-a-list-in-tkinter
-        self.canvas.create_oval(x - 10, y - 10,
-                                x + 10, y + 10,
-                                fill='red', width=1, tags='corners')
+        self._canvas.create_oval(x - 10, y - 10,
+                                 x + 10, y + 10,
+                                 fill='red', width=1, tags='corners')
 
         # add tuple (x, y) to existing list
-        self.clicks.append((x, y))
+        self.__clicks.append((x, y))
 
-        if len(self.clicks) >= 4:
-            rect = np.asarray([np.asarray(np.float32(p)) for p in
-                               self.clicks])
-            flat = nPTransform.four_points_correct_aspect(self.cv_img,
-                                                          rect,
-                                                          self.dim[0],
-                                                          self.dim[1])
+        if len(self.__clicks) >= 4:
+            self._img_flattened = True
+            self.request_dims()
 
-            self.canvas.delete('corners')
-            self.save_img_to_folder_with_extra('_flat', flat)
-            self.load_next_img_or_end()
+    def _flatten_img(self):
+        """flatten the current img
 
-            self.clicks = []
+        Precondition:
+        len(self._clicks) == 4
+        """
+        assert len(self.__clicks) == 4
+        rect = np.asarray([np.asarray(np.float32(p)) for p in
+                           self.__clicks])
+
+        flat = nPTransform.four_points_correct_aspect(self.final_cv_img,
+                                                      rect,
+                                                      self.__dim[0],
+                                                      self.__dim[1])
+
+        self._canvas.delete('corners')
+
+        self.show_cv_image(flat)
+        self.final_cv_img = flat
+
+        self._canvas.xview_moveto(0)
+        self._canvas.yview_moveto(0)
+
+        self.__clicks = []
+
+    def reload(self):
+        """Need to reset _img_flattened"""
+        self._img_flattened = False
+        self.__clicks = []
+        self._canvas.delete('corners')
+        Image_GUI.reload(self)
+
+    def next_file(self):
+        """Runs when next button pressed saves image to save_to with prefix
+        'flat' and loads the next image"""
+        self._img_flattened = False
+        self.__num_saved = 0
+        self.load_next_img_or_end()
+
+    def save(self):
+        """Save the current image"""
+        if self._img_flattened:
+            if self.__num_saved > 0:
+                path = self.save_img_to_folder_with_extra(
+                    f'_flat_{self._num_saved}')
+            else:
+                path = self.save_img_to_folder_with_extra(f'_flat')
+            self.__saved[path] = self.__dim
+            self.__num_saved += 1
+            self.reload()
+
+    def request_dims(self):
+        """Creates pop up requesting dimensions"""
+        pop = Tk()
+        pop.wm_title('Need dimensions')
+
+        long_label = Label(pop, text='Longer side (cm)')
+        long_label.pack()
+        long_entry = Entry(pop)
+        long_entry.pack()
+        short_labe = Label(pop, text='Sorter side (cm)')
+        short_labe.pack()
+        short_entry = Entry(pop)
+        short_entry.pack()
+
+        def skip():
+            pop.destroy()
+            self._flatten_img()
+
+        def done():
+            short_str = short_entry.get()
+            long_str = long_entry.get()
+
+            try:
+                short = float(short_str)
+            except ValueError:
+                print('That\'s not a number.')
+                return
+            try:
+                long = float(long_str)
+            except ValueError:
+                print('That\'s not a number.')
+                return
+
+            self.__dim = (short, long)
+
+            pop.destroy()
+            self._flatten_img()
+
+        done_button = Button(pop, text='Done', command=done)
+        done_button.pack(side=LEFT)
+
+        if self.__dim is not None:
+            skip_button = Button(pop, text='Use Last',
+                                 command=skip)
+            skip_button.pack(side=LEFT)
+
+        pop.mainloop()
 
 
 if __name__ == '__main__':
-    root = Tk()
-    pro = Image_Falattener(root, ['../Sample_Images/solar.jpg', '../Sample_Images/book.png'], 'test/', (215.9, 279.4))
-    #cv2.imwrite("result.png", pro.process_image_at('Sample_Images/solar.jpg'))
-    root.mainloop()
+    parser = argparse.ArgumentParser(description='Flatten a list of panels')
+    parser.add_argument('--input', type=str,
+                        help='The path to the jason contain')
+    parser.add_argument('--output', type=str,
+                        help='The folder to save output json and new images')
 
+    arg = parser.parse_args()
 
+    master = Tk()
+
+    with open(arg.input) as input_json:
+        images_dict = json.load(input_json)
+
+    flattener = ImageFlattener(master, list(images_dict.keys()), arg.output)
+
+    master.mainloop()
+
+    if not os.path.exists(arg.output):
+        os.makedirs(arg.output)
+
+    with open(os.path.join(arg.output, 'result.json'), 'w') as outfile:
+        json.dump(flattener.saved, outfile)
--- src/task2/flattening_module/image_gui.py
+++ src/task2/flattening_module/image_gui.py
@@ -11,26 +11,23 @@ MOVE_SPEED = 10
 class Image_GUI:
     """This class represents an the image processing application
 
-    ==== Atributes ===
-    master: the root of the gui
-    canvas: the canvas containing the image being edited
-    final_cv_img: The final image to be saved in the form of a np.array
-    tk_image: the image currently being displayed on the screen.
-    im_on_canvas: id of the image on the canvas.
-    paths: A list of all images to be loaded
-    _img_counter: Index of current image in paths being used.
+    ==== Properties ===
     save_to: the path to which the images will be saved
-    im_on_canvas: the id of the image on the canvas
     """
-    master: Tk
-    canvas: Canvas
-    final_cv_img: np.ndarray
-    tk_image: PhotoImage
-    paths: List[str]
-    _img_counter: int
-    save_to: str
-    _x_slider: Scrollbar
-    _y_slider: Scrollbar
+    # === Private Attributes ===
+    _save_to: str  # the path to which the images will be saved
+    _master: Tk  # the root of the gui
+    _canvas: Canvas  # the canvas containing the image being edited
+    _final_cv_img: np.ndarray  # The final image to be saved in the form of
+    # a np.array
+    _paths: List[str]  # A list of all images to be loaded
+    # im_on_canvas: the id of the image on the canvas TODO
+
+    __tk_image: PhotoImage  # the image currently being displayed on the screen.
+    __img_counter: int  # _img_counter: Index of current image in paths being
+    # used.
+    __x_slider: Scrollbar
+    __y_slider: Scrollbar
 
     def __init__(self, master: Tk, paths: List[str], save_to: str):
         """
@@ -40,44 +37,49 @@ class Image_GUI:
 
         Precondition: Paths is not paths is not empty.
         """
-        self.master = master
-        self.paths = paths
-        self._img_counter = 0
-        self.save_to = save_to
+        self._master = master
+        self._paths = paths
+        self.__img_counter = 0
+        self._save_to = save_to
 
         # Create image frame and canvas
-        photo_frame = Frame(self.master, bd=2, relief=SUNKEN)
+        photo_frame = Frame(self._master, bd=2, relief=SUNKEN)
         photo_frame.grid_rowconfigure(0, weight=1)
         photo_frame.grid_columnconfigure(0, weight=1)
 
-        self._x_slider = Scrollbar(photo_frame, orient=HORIZONTAL)
-        self._y_slider = Scrollbar(photo_frame)
+        self.__x_slider = Scrollbar(photo_frame, orient=HORIZONTAL)
+        self.__y_slider = Scrollbar(photo_frame)
 
-        self.canvas = Canvas(photo_frame, bd=0,
-                             xscrollcommand=self._x_slider.set,
-                             yscrollcommand=self._y_slider.set)
+        self._canvas = Canvas(photo_frame, bd=0,
+                              xscrollcommand=self.__x_slider.set,
+                              yscrollcommand=self.__y_slider.set)
 
-        self._x_slider.config(command=self.canvas.xview)
-        self._y_slider.config(command=self.canvas.yview)
-        self.canvas.config(scrollregion=self.canvas.bbox("all"))
+        self.__x_slider.config(command=self._canvas.xview)
+        self.__y_slider.config(command=self._canvas.yview)
+        self._canvas.config(scrollregion=self._canvas.bbox("all"))
 
         # Place image
 
-        self._x_slider.grid(row=1, column=0, sticky=E + W)
-        self._y_slider.grid(row=0, column=1, sticky=N + S)
-        self.canvas.grid(row=0, column=0, sticky=N + S + E + W)
+        self.__x_slider.grid(row=1, column=0, sticky=E + W)
+        self.__y_slider.grid(row=0, column=1, sticky=N + S)
+        self._canvas.grid(row=0, column=0, sticky=N + S + E + W)
         photo_frame.pack(fill=BOTH, expand=1)
 
-        self.final_cv_img = cv2.imread(paths[0])
-        self.tk_image = self.cv2tk_image(self.final_cv_img)
+        self._final_cv_img = cv2.imread(paths[0])
+        self.__tk_image = self.cv2tk_image(self._final_cv_img)
 
-        self.im_on_canvas = self.canvas.create_image(0, 0, image=self.tk_image,
-                                                     anchor="nw",
-                                                     tags='canvas_image')
+        self.im_on_canvas = self._canvas.create_image(0, 0, image=self.__tk_image,
+                                                      anchor="nw",
+                                                      tags='canvas_image')
+        print("type of im on canvas", type(self.im_on_canvas))
+
+    @property
+    def save_to(self):
+        return self._save_to
 
     def _up(self):
         """called on up arrow press"""
-        self._x_slider.set(0.5, 0.5)
+        self.__x_slider.set(0.5, 0.5)
     def _down(self):
         """called on down arrow press"""
 
@@ -97,54 +99,54 @@ class Image_GUI:
         """Takes a image in array fromat and sets the image on the canvas
         to it"""
         new_tkimage = self.cv2tk_image(cv_img)
-        self.canvas.create_image(0, 0, image=self.tk_image,
-                                 anchor="nw",
-                                 tags='canvas_image')
-        self.canvas.itemconfig(self.im_on_canvas, image=new_tkimage)
-        self.tk_image = new_tkimage
+        self._canvas.create_image(0, 0, image=self.__tk_image,
+                                  anchor="nw",
+                                  tags='canvas_image')
+        self._canvas.itemconfig(self.im_on_canvas, image=new_tkimage)
+        self.__tk_image = new_tkimage
 
     def to_canvas(self, point: Tuple[float, float]) -> Tuple[int, int]:
         """Takes a point in the window and converts it to canvas space."""
-        return int(self.canvas.canvasx(point[0])),\
-            int(self.canvas.canvasy(point[1]))
+        return int(self._canvas.canvasx(point[0])),\
+            int(self._canvas.canvasy(point[1]))
 
-    def save_img_to_folder_with_extra(self, extra: str):
+    def save_img_to_folder_with_extra(self, extra: str) -> str:
         """Save the final_cv_img to the save_to folder
         with <extra> added to its name"""
-        if not os.path.exists(self.save_to):
-            os.makedirs(self.save_to)
+        if not os.path.exists(self._save_to):
+            os.makedirs(self._save_to)
 
-        name = os.path.basename(self.paths[self._img_counter])
+        name = os.path.basename(self._paths[self.__img_counter])
 
         slit = name.split('.')
 
         if len(slit) > 1:
             sections = slit[:-1]
             extension = slit[-1]
-            path = os.path.join(self.save_to, '.'.join(sections) + extra + '.' + extension)
-            cv2.imwrite(path, self.final_cv_img)
+            path = os.path.join(self._save_to, '.'.join(sections) + extra + '.'
+                                + extension)
+            cv2.imwrite(path, self._final_cv_img)
         else:
-            path = os.path.join(self.save_to, name + extra)
-            cv2.imwrite(path, self.final_cv_img)
+            path = os.path.join(self._save_to, name + extra)
+            cv2.imwrite(path, self._final_cv_img)
+
+        return path
 
     def load_next_img_or_end(self):
         """Loads next image from paths if there are no more images to load
         it ends the main loop.
         """
-        if self._img_counter < len(self.paths) - 1:
-            self._img_counter += 1
+        if self.__img_counter < len(self._paths) - 1:
+            self.__img_counter += 1
             self.reload()
         else:
-            self.master.destroy()
+            self._master.destroy()
 
     def reload(self):
         """reload the current image from the path"""
-        self.final_cv_img = cv2.imread(self.paths[self._img_counter])
-        self.show_cv_image(self.final_cv_img)
+        self._final_cv_img = cv2.imread(self._paths[self.__img_counter])
+        self.show_cv_image(self._final_cv_img)
 
-
-if __name__ == '__main__':
-    root = Tk()
-    pro = Image_GUI(root, ['Sample_Images/Test_Images/2018-05-25_16-22-29-018.bmp'], 'test')
-    #cv2.imwrite("result.png", pro.process_image_at('Sample_Images/solar.jpg'))
-    root.mainloop()
+    def reset_scroll(self):
+        self._canvas.config(xscrollcommand=self.__x_slider.set)
+        self._canvas.config(yscrollcommand=self.__y_slider.set)
