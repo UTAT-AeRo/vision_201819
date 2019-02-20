d924c38835df2ab52d6aab910be0f110f061d6d5
Lev McKinney <levmckinney@gmail.com>
2019-02-19 19:25:34 -0500

    improved documentation and added new arg decimal places

--- src/task2/damage_selection_gui/README.md
+++ src/task2/damage_selection_gui/README.md
@@ -3,9 +3,10 @@ Allows for the annotation of images of panels.
 
 
 ### USAGE
+```
 usage: damageselector.py [-h] [--input INPUT] [--output OUTPUT]
                          [--line_width LINE_WIDTH] [--text_size TEXT_SIZE]
-                         [--digits DIGITS]
+                         [--decimal_places DECIMAL_PLACES]
 
 Flatten a list of panels
 
@@ -18,10 +19,12 @@ optional arguments:
                         corners.
   --text_size TEXT_SIZE
                         The size of the text on the final cv image
-  --digits DIGITS
+  --decimal_places DECIMAL_PLACES
+                        The number of decimal points for lengths and areas
 
-
-
+Process finished with exit code 0
+```
+s
 ### Input Format
 ```json
 [
--- src/task2/damage_selection_gui/damageselector.py
+++ src/task2/damage_selection_gui/damageselector.py
@@ -8,19 +8,20 @@ from enum import Enum
 from shapely.geometry import Polygon
 from imageprocessor import Panel, ImageProcessor, JsonFormatError
 
-# https://stackoverflow.com/questions/5501192/how-to-display-picture-and-get-mouse-click-coordinate-on-it
-
-LINE_COLOR = (0, 0, 255)
+# Defaults for args
 LINE_WIDTH = 2
+DECIMAL_PLACES = 0
 TEXT_SIZE = 0.5
+
+# Constants
+LINE_COLOR = (0, 0, 255)
+TEXT_THICKNESS = 2
 TEXT_COLOR = (0, 125, 200)
 TEXT_FONT = cv2.FONT_HERSHEY_SIMPLEX
-TEXT_THICKNESS = 2
 MARK_TAG = "mark"
-CANVAS_LINE_COLOR = 'red'
-CANVAS_LINE_THICKNESS = 2
 CANVAS_DASH_PATTERN = (5,)
-DECIMAL_PLACES = 0
+CANVAS_LINE_COLOR = 'red'
+CANVAS_LINE_WIDTH = LINE_WIDTH
 
 
 class State(Enum):
@@ -30,10 +31,17 @@ class State(Enum):
 
 
 class DamageSelector(ImageProcessor):
-    """"""
+    """Class for selecting and identifying damaged area
+    """
+    __state: State
+    __panel_in: List[Panel]  # A list of panels input to the class these include
+    # location, dimensions and path to an image of this.
+    __clicks: List[Tuple[int, int]]  # list of all clicks since last reset.
+    __state_label: Label  # The label at the top of the window displaying the
+    # state of the DamageSelector
 
     def __init__(self, master: Tk, panels_in: List[Panel], save_to):
-        """"""
+        """Create a new Damage selector"""
 
         self.__state = State.DEFAULT
         self.__panels_in = panels_in
@@ -95,7 +103,7 @@ class DamageSelector(ImageProcessor):
 
     def left_mouse_move(self, event):
         """Called whenever left mouse is moved while being held down
-           on canvas.
+        on canvas.
         """
         x, y = self.movable_image.win_to_canvas((event.x, event.y))
         self._clear_canvas()
@@ -107,28 +115,39 @@ class DamageSelector(ImageProcessor):
     def _clear_canvas(self):
         self.movable_image.canvas.delete(MARK_TAG)
 
+    # The following to methods do not directly edit the final image.
+    # They add lines and circles to the canvas to preview the next part of the
+    # selection.
+
+    def _draw_line_on_canvas_from_last_click(self, point: Tuple[int, int]):
+        """Draws a line from the last point in the clicks list to a point on
+        the point given on the canvas. <point> is in canvas cords.
+        """
+        if len(self.__clicks) >= 1:
+            p1 = self.movable_image.cv_to_canvas(self.__clicks[-1])
+            p2 = point
+            self.movable_image.canvas.create_line(p1[0], p1[1],
+                                                  p2[0], p2[1],
+                                                  width=CANVAS_LINE_WIDTH,
+                                                  tags=MARK_TAG,
+                                                  dash=CANVAS_DASH_PATTERN,
+                                                  fill=CANVAS_LINE_COLOR)
+
     def _draw_circle_on_canvas_from_last_click(self, point: Tuple[int, int]):
+        """Draws a circle with its edge being the last point in the clicks list
+        and its center being the point provided on the canvas.
+        <point> is in canvas cords.
+        """
         if len(self.__clicks) >= 1:
             p1 = self.movable_image.cv_to_canvas(self.__clicks[-1])
             p2 = point
             r = self._dist(p1, p2)
             self.movable_image.canvas.create_oval(p2[0] - r, p2[1] - r,
                                                   p2[0] + r, p2[1] + r,
-                                                  width=CANVAS_LINE_THICKNESS,
+                                                  width=CANVAS_LINE_WIDTH,
                                                   tags=MARK_TAG,
                                                   dash=CANVAS_DASH_PATTERN,
                                                   outline=CANVAS_LINE_COLOR)
-
-    def _draw_line_on_canvas_from_last_click(self, point: Tuple[int, int]):
-        if len(self.__clicks) >= 1:
-            p1 = self.movable_image.cv_to_canvas(self.__clicks[-1])
-            p2 = point
-            self.movable_image.canvas.create_line(p1[0], p1[1],
-                                                  p2[0], p2[1],
-                                                  width=CANVAS_LINE_THICKNESS,
-                                                  tags=MARK_TAG,
-                                                  dash=CANVAS_DASH_PATTERN,
-                                                  fill=CANVAS_LINE_COLOR)
 
     def left_mouse_up(self, event):
         """Called when ever left mouse button is released"""
@@ -141,14 +160,22 @@ class DamageSelector(ImageProcessor):
             if len(self.__clicks) >= 4:
                 self._draw_line_on_cv_from_last_click(self.__clicks[0])
 
-                print('Side 1:', self._dist(self.__clicks[0], self.__clicks[1]) * self.px_size(), 'cm')
-                print('Side 2:', self._dist(self.__clicks[1], self.__clicks[2]) * self.px_size(), 'cm')
-                print('Side 3:', self._dist(self.__clicks[2], self.__clicks[3]) * self.px_size(), 'cm')
-                print('Side 4:', self._dist(self.__clicks[3], self.__clicks[0]) * self.px_size(), 'cm')
+                print('Side 1:', self._dist(self.__clicks[0],
+                                            self.__clicks[1]) * self.px_size(),
+                      'cm')
+                print('Side 2:', self._dist(self.__clicks[1],
+                                            self.__clicks[2]) * self.px_size(),
+                      'cm')
+                print('Side 3:', self._dist(self.__clicks[2],
+                                            self.__clicks[3]) * self.px_size(),
+                      'cm')
+                print('Side 4:', self._dist(self.__clicks[3],
+                                            self.__clicks[0]) * self.px_size(),
+                      'cm')
 
                 quadrangle = Polygon(self.__clicks)
 
-                print('Area:', quadrangle.area * self.px_size()**2, 'cm^2')
+                print('Area:', quadrangle.area * self.px_size() ** 2, 'cm^2')
 
                 self.enter_default_state()
         elif self.__state == State.CIRCLE:
@@ -168,6 +195,10 @@ class DamageSelector(ImageProcessor):
                 self.enter_default_state()
 
     def _draw_line_on_cv_from_last_click(self, point):
+        """Draws a line from the last point in the clicks list to a point on
+        the point given on the cv image that will be saved.
+        <point> is in canvas cords.
+        """
         if len(self.__clicks) >= 1:
 
             cv2.line(self.movable_image.cv_img, self.__clicks[-1], point,
@@ -176,7 +207,9 @@ class DamageSelector(ImageProcessor):
             text_org = ((point[0] + self.__clicks[-1][0]) // 2,
                         (point[1] + self.__clicks[-1][1]) // 2 + int(TEXT_SIZE))
 
-            length = np.round(self._dist(self.__clicks[-1], point) * self.px_size(), DECIMAL_PLACES)
+            length = np.round(
+                self._dist(self.__clicks[-1], point) * self.px_size(),
+                DECIMAL_PLACES)
 
             cv2.putText(self.movable_image.cv_img,
                         f'{length}cm'
@@ -186,6 +219,10 @@ class DamageSelector(ImageProcessor):
             self.movable_image.refresh()
 
     def _draw_circle_on_cv_from_last_click(self, center):
+        """Draws a circle with its edge being the last point in the clicks list
+        and its center being the point provided on the cv image that will be
+        saved. <point> is in cv image cords.
+        """
         if len(self.__clicks) >= 1:
             cv2.circle(self.movable_image.cv_img, center,
                        int(self._dist(center, self.__clicks[0])),
@@ -199,15 +236,16 @@ class DamageSelector(ImageProcessor):
         """
         x, y, _ = self.movable_image.cv_img.shape
 
-        px_size = self.curr_panel.dims[0]/x
+        px_size = self.curr_panel.dims[0] / x
 
         return px_size
 
-    def _dist(self, point1: tuple, point2: tuple) -> int:
+    @staticmethod
+    def _dist(point1: tuple, point2: tuple) -> int:
         """Returns the distance in px between two points on the canvas"""
         delta_x = abs(point1[0] - point2[0])
         delta_y = abs(point1[1] - point2[1])
-        return np.sqrt(delta_x**2 + delta_y**2)
+        return np.sqrt(delta_x ** 2 + delta_y ** 2)
 
 
 def parse_input(panel_listings: any) -> List[Panel]:
@@ -220,7 +258,7 @@ def parse_input(panel_listings: any) -> List[Panel]:
 
     for image_listing in panel_listings:
         if ('file' not in image_listing or
-            'gps' not in image_listing or
+                'gps' not in image_listing or
                 'pixel' not in image_listing):
             raise JsonFormatError('All items must contain a \"file\", \
                                   \"gps\" and \"pixle\" keys.')
@@ -233,8 +271,9 @@ def parse_input(panel_listings: any) -> List[Panel]:
         if not isinstance(file, str):
             raise JsonFormatError('File must be string')
         if not isinstance(gps, list) or not isinstance(pixel, list) \
-            or not isinstance(dims, list) or len(gps) != 2 or len(pixel) != 2\
-            or len(dims) != 2:
+                or not isinstance(dims, list) or len(gps) != 2 or len(
+            pixel) != 2 \
+                or len(dims) != 2:
             raise JsonFormatError('\"gps\" and \"pixel 2\" should be a list \
                                    of length 2')
 
@@ -253,20 +292,24 @@ if __name__ == '__main__':
                         help='The size of the dots used to mark and select \
                         panel corners.',
                         default=LINE_WIDTH)
-    parser.add_argument('--text_size', type=int, help='The size of the text on \
+    parser.add_argument('--text_size', type=float, help='The size of the text on \
                         the final cv image', default=TEXT_SIZE)
-    parser.add_argument('--digits')
+    parser.add_argument('--decimal_places', type=int, float='The number of \
+                        decimal points for lengths and areas',
+                        default=DECIMAL_PLACES)
 
-    arg = parser.parse_args()
+    args = parser.parse_args()
 
-    LINE_WIDTH = arg.line_width
-    CANVAS_LINE_THICKNESS = arg.line_width
+    LINE_WIDTH = args.line_width
+    CANVAS_LINE_WIDTH = args.line_width
+    TEXT_SIZE = args.text_size
+    DECIMAL_PLACES = args.decimal_places
 
-    with open(arg.input) as input_json:
+    with open(args.input) as input_json:
         panel_listings = json.load(input_json)
 
     panels_in = parse_input(panel_listings)
 
     root = Tk()
-    pro = DamageSelector(root, panels_in, "test")
+    pro = DamageSelector(root, panels_in, args.output)
     root.mainloop()
