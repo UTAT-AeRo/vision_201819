e52b4c40b0f2dbfcc02a21769ca28624863c9a8f
az20304 <zhual@utschools.ca>
2019-03-31 12:33:52 -0400

    lint

--- src/task1/idsignificantchanges/idsignificantchanges.py
+++ src/task1/idsignificantchanges/idsignificantchanges.py
@@ -1,16 +1,18 @@
 import json
 import tkinter
 import argparse
-import PIL.Image, PIL.ImageTk
+import PIL.Image
+import PIL.ImageTk
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-#written by Alex Zhuang zhual@utschools.ca 2019/02/23
+#  written by Alex Zhuang zhual@utschools.ca 2019/02/23
 
-global index, basewidth, out, cycle, cyclecanvas, lat, long, second, canvas, t, point
+global index, basewidth, out, lat, long
+global cycle, cyclecanvas, canvas, t, point, second
 index = 0
 basewidth = 300
-out = {'damaged':[]}
+out = {'damaged': []}
 
 
 def output():
@@ -28,8 +30,8 @@ def prev():
     decrements image index within inputsurveyphotos list
     """
     global index
-    if (index >0):
-        index -=1
+    if (index > 0):
+        index -= 1
         refresh()
 
 
@@ -43,7 +45,7 @@ def next():
         refresh()
 
 
-def refresh(init = False):
+def refresh(init=False):
     """
     Refresh the image in the image viewing window based on next or
     previous buttons. Resizes images if too large.
@@ -54,13 +56,13 @@ def refresh(init = False):
     cycleimg = PIL.Image.open(inputsurveyphotos[index])
     wpercent = (basewidth/float(cycleimg.size[0]))
     hsize = int((float(cycleimg.size[1])*float(wpercent)))
-    cycleimg = cycleimg.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
-    cyclephoto =PIL.ImageTk.PhotoImage(cycleimg)
+    cycleimg = cycleimg.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+    cyclephoto = PIL.ImageTk.PhotoImage(cycleimg)
     cycle.cyclephoto = cyclephoto
     cycle.geometry('{}x{}'.format(cyclephoto.width(), cyclephoto.height()+80))
-    if init == True:
-        cyclecanvas = tkinter.Canvas(cycle, width = cyclephoto.width(),
-                                            height = cyclephoto.height())
+    if init:
+        cyclecanvas = tkinter.Canvas(cycle, width=cyclephoto.width(),
+                                     height=cyclephoto.height())
     cyclecanvas.create_image(0, 0, image=cyclephoto, anchor=tkinter.NW)
     cyclecanvas.pack()
 
@@ -72,10 +74,10 @@ def save():
     global index, lat, long, second, t, point
     desc = t.get("1.0", 'end-1c')
     out['damaged'].append({'lat': lat,
-                            'long': long,
-                            'message': desc,
-                            'filename': inputsurveyphotos[index]})
-    print ("the current list of points is as follows:\n")
+                           'long': long,
+                           'message': desc,
+                          'filename': inputsurveyphotos[index]})
+    print("the current list of points is as follows:\n")
     for each in out['damaged']:
         print(each)
     second.destroy()
@@ -102,25 +104,40 @@ def clicked(event):
     -pops up window to add description to lat, long value.
     -If save and quit is clicked, saves the point.
 
+    ** We assume that the image is oriented North
+
+    lat calculation:
+    (relative y of point in image * distance between latitude coordinates)
+    + most southern latitude coordinate
+
+    long calculation:
+    (relative x of point in image * distance between longitude coordinates)
+    + most western longitude coordinate
     """
     global index, lat, long, second, t, point
-    if (event.x >= 0 and event.x <= photo.width() and event.y >= 0 and event.y <= photo.height()):
-        second = tkinter.Toplevel(width = 300)
+    if (event.x >= 0 and event.x <= photo.width() and event.y >= 0
+            and event.y <= photo.height()):
+        second = tkinter.Toplevel(width=300)
 
         x1, y1 = (event.x - 1), (event.y - 1)
         x2, y2 = (event.x + 1), (event.y + 1)
 
-        #Assumed that image is oriented North
-        #(relative y of point in image * distance between latitude coordinates) + most southern latitude coordinate
-        lat = (((photo.height()-event.y)/photo.height())*abs(inputmap['topleft']['lat']-inputmap['bottomright']['lat']))+inputmap['bottomright']['lat']
-        #(relative x of point in image * distance between longitude coordinates) + most western longitude coordinate
-        long = ((event.x/photo.width())*abs(inputmap['topleft']['long']-inputmap['bottomright']['long']))+inputmap['topleft']['long']
-        point = canvas.create_oval(x1, y1, x2, y2, fill="#FF0000", outline="#FF0000", width=10)
+        lat = ((((photo.height()-event.y)/photo.height()) *
+               abs(inputmap['topleft']['lat']-inputmap['bottomright']['lat']))
+               + inputmap['bottomright']['lat'])
+        long = (((event.x/photo.width()) *
+                abs(inputmap['topleft']['long']
+                - inputmap['bottomright']['long']))
+                + inputmap['topleft']['long'])
+        point = canvas.create_oval(x1, y1, x2, y2, fill="# FF0000",
+                                   outline="# FF0000", width=10)
         second.grab_set()
         second.title("New Label")
 
-        #message box
-        msg = tkinter.Message(second, justify = 'center', width = 300, text="Label at (lat,long) \n{}, {}".format(lat, long))
+        # message box
+        msg = tkinter.Message(second, justify='center', width=300,
+                              text="Label at (lat,long) \n{}, {}"
+                              .format(lat, long))
         msg.pack()
         t = tkinter.Text(second, height=5, width=40)
         t.pack()
@@ -132,45 +149,49 @@ def clicked(event):
         second.mainloop()
 
 
-#Argparse section
-parser = argparse.ArgumentParser(description='You must specify paths to two jsons')
-parser.add_argument("--map", help = 'valid path to the reference map json', type = str)
-parser.add_argument("--photos", help = 'valid path to the survey photos json', type = str)
+# Argparse section
+parser = argparse.ArgumentParser(
+                    description='You must specify paths to two jsons')
+parser.add_argument("--map", help='valid path to the reference map json',
+                    type=str)
+parser.add_argument("--photos", help='valid path to the survey photos json',
+                    type=str)
 args = parser.parse_args()
 mappath = args.map
 photopath = args.photos
 
-#read paths provided by command line arguments e.g.
-#python idsignificantchanges.py --map=inputmap.json --photos=inputsurveyphotos.json
+# read paths provided by command line arguments e.g.
+# python idsignificantchanges.py --map=inputmap.json
+# --photos=inputsurveyphotos.json
 with open(mappath, 'r') as f:
     inputmap = json.load(f)
 with open(photopath, 'r') as f:
     inputsurveyphotos = json.load(f)
 
-#Create tk instance and set it to output the json on window close.
+# Create tk instance and set it to output the json on window close.
 top = tkinter.Tk()
 top.protocol("WM_DELETE_WINDOW", output)
 
-#Open the map image and fit the window to the image dims
-img = PIL.Image.open(inputmap['filename']);
-photo =PIL.ImageTk.PhotoImage(img)
-top.geometry('{}x{}'.format(photo.width(), photo.height()) )
+# Open the map image and fit the window to the image dims
+img = PIL.Image.open(inputmap['filename'])
+photo = PIL.ImageTk.PhotoImage(img)
+top.geometry('{}x{}'.format(photo.width(), photo.height()))
 
-#Place a canvas over the entire window
-canvas = tkinter.Canvas(top, width = photo.width(), height = photo.height())
+# Place a canvas over the entire window
+canvas = tkinter.Canvas(top, width=photo.width(), height=photo.height())
 canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
 canvas.pack()
 
-#on left-click on canvas, label a point
+# on left-click on canvas, label a point
 canvas.bind("<Button-1>", clicked)
 
-#Second tk window for viewing inputsurveyphotos images and cycling through
+# Second tk window for viewing inputsurveyphotos images and cycling through
 cycle = tkinter.Toplevel()
-#Window inialization
+# Window inialization
 init = True
 refresh(init)
 
-#Cycle between images in the list using next and previous buttons
+# Cycle between images in the list using next and previous buttons
 next = tkinter.Button(cycle, text="Next", command=next)
 next.pack()
 prev = tkinter.Button(cycle, text="Previous", command=prev)
